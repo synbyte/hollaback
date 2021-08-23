1029ca8bd21a57712fff2885537bd69401ab9261
synbyte <rashnikov2.0@gmail.com>
2021-08-23 10:59:47 -0700

    3 files modified

--- src/list/list.component.ts
+++ src/list/list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class ListComponent implements OnInit {
-messageList = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repr","hi aliquip ex ea commodo consequat. Dui","ehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."];
+messageList = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repr","hi aliquip ex ea commodo consequat. Dui","ehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.","henderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt","henderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt"];
   constructor() { }
 
   ngOnInit() {
